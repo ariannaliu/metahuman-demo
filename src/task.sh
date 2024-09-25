529ddc4074312fdd6c501eabf7f0f6baaad71f8b
Zhixuan Liu <118010202@link.cuhk.edu.cn>
2024-09-25 15:42:05 -0400

    able to train sdxl lora

--- src/task.sh
+++ src/task.sh
@@ -1,11 +1,11 @@
 export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
 export VAE_NAME="madebyollin/sdxl-vae-fp16-fix"
-export DATASET_NAME="lambdalabs/naruto-blip-captions"
+export DATASET_NAME="/home/zhixuan/demo/metahuman-demo/data/cctv"
 
 accelerate launch train_text_to_image_lora_sdxl.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --pretrained_vae_model_name_or_path=$VAE_NAME \
-  --dataset_name=$DATASET_NAME \
+  --train_data_dir=$DATASET_NAME \
   --caption_column="text" \
   --resolution=1024 --random_flip \
   --train_batch_size=1 \
@@ -13,6 +13,6 @@ accelerate launch train_text_to_image_lora_sdxl.py \
   --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
   --seed=42 \
-  --output_dir="sd-naruto-model-lora-sdxl" \
-  --validation_prompt="cute dragon creature" \
+  --output_dir="sd-cctv-model-lora-sdxl" \
+  --validation_prompt="a CCTV-TVCC40011 camera in pink color" \
   --report_to="wandb"
